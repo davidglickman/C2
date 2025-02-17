8e8357d055348e8c0853ca6ceab91c8f83c42d20
davidglickman <37016306+davidglickman@users.noreply.github.com>
2025-02-17 09:28:01 +0200

    Add files via upload

---
+++ frontend/src/components/ui/Button.js
@@ -0,0 +1,16 @@
+// frontend/src/components/ui/button.js
+
+import React from "react";
+
+const Button = ({ onClick, children, className = "" }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`px-4 py-2 bg-blue-500 text-white rounded ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
